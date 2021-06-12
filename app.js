e668cb736969392321c23cb39b30e10ad5e7906c
Matheus <matheus081105@gmail.com>
2021-06-12 00:22:13 -0300

    .

--- app.js
+++ app.js
@@ -12,18 +12,39 @@ $('.blade1').click(function() {
       on.play();
     }
   })
-  
-  $('.blade2').click(function() {
-    $('body').toggleClass('blade2');
-      let on = $('#on')[0];
-    if($('body').hasClass('blade2')) {
-      $('.blade2 .sabre2, .blade2 .sombra2').css({
-        'height': '0px'
-      })
-    }else{
-      $('.blade2 .sabre2, .blade2 .sombra2').css({
-        'height': '200px'
-      })
-      on.play();
-    }
+
+  var color1 = $('#1');
+  var color2 = $('#2');
+  var color3 = $('#3');
+  var color4 = $('#4');
+
+  $(color1).click(function(){
+    document.documentElement.style.setProperty('--color', 'hsl(120, 100%, 50%)');
+  });
+
+  $(color2).click(function(){
+    document.documentElement.style.setProperty('--color', 'hsl(0, 100%, 50%)');
   })
+
+  $(color3).click(function(){
+    document.documentElement.style.setProperty('--color', 'hsl(25, 100%, 50%)');
+  })
+
+  $(color4).click(function(){
+    document.documentElement.style.setProperty('--color', 'hsl(50, 100%, 50%)');
+  })
+  
+  // $('.blade2').click(function() {
+  //   $('body').toggleClass('blade2');
+  //     let on = $('#on')[0];
+  //   if($('body').hasClass('blade2')) {
+  //     $('.blade2 .sabre2, .blade2 .sombra2').css({
+  //       'height': '0px'
+  //     })
+  //   }else{
+  //     $('.blade2 .sabre2, .blade2 .sombra2').css({
+  //       'height': '200px'
+  //     })
+  //     on.play();
+  //   }
+  // })
